65287f800a3d03665f943d0076572ffc8882a4b2
Viciot <vfuentesnoguera@gmail.com>
2021-06-15 19:08:05 +0200

    Co-authored-by: Marco Santonastasi <marco.santonastasi@altesis-pro.com>

--- Aguafiestas.js
+++ Aguafiestas.js
@@ -1,51 +1,78 @@
 /* Defino Array de posiciones */
-const tablero = [
-	["o", "x", "x", "x", "x"],
-	["o", "x", "o", "o", "x"],
-	["o", "x", "x", "x", "x"],
-	["x", "x", "x", "o", "o"],
-	["x", "x", "x", "x", "x"]
+const myTablero = [
+	[1, 0, 0, 0, 0],
+	[1, 0, 2, 2, 0],
+	[1, 0, 0, 0, 0],
+	[0, 0, 0, 3, 3],
+	[0, 0, 0, 0, 0]
 ]
+const rivalHits = [] 
+
+const tableroRival = [
+	[1, 1, 1, 0, 0],
+	[0, 0, 0, 2, 2],
+	[0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 1],
+	[0, 0, 0, 0, 1]
+]
+const myHits = [] 
+
+
+function iAmHit(event){
+	console.log("You clicked  " + event.target.getAttribute("data-ship-id"))
+}
+
+function rivalIsHit(event){
+	console.log("You clicked  " + event.target.getAttribute("data-ship-id"))
+}
+
+/* Una vez creamos todo el codigo hacemos esto */
+function Cargado(){
 
 /* Creo tablero de la izquierda */
 var ContenedorMiTablero = document.createElement('div');
 ContenedorMiTablero.id = 'MiTablero';
-var MiTablero = document.getElementById('MiTablero'); 
+ContenedorMiTablero.addEventListener("click", iAmHit )
 document.body.appendChild(ContenedorMiTablero); 
 
 /* Creo tablero de la derecha */
 var ContenedorTableroRival = document.createElement('div');
 ContenedorTableroRival.id = 'TableroRival';
-var TableroRival = document.getElementById('TableroRival'); 
+ContenedorTableroRival.addEventListener("click", rivalIsHit )
 document.body.appendChild(ContenedorTableroRival); 
 
 
-/* Pinto Celdas de tablero */
-for(var fila=0; fila<5; fila++) {
-    for(var columna=0; columna<5; columna++) {
+/* Pinto Celdas de MiTablero */
+for(let fila=0; fila<5; fila++) {
+    for(let columna=0; columna<5; columna++) {
 
     	var Celda = document.createElement('div');
 		Celda.className = 'Celda';
+		Celda.setAttribute("data-ship-id", myTablero[fila][columna] )
     	ContenedorMiTablero.append(Celda);
 
     	let p = document.createElement('p')
-		Celda.append(tablero[fila][columna], p);
+		Celda.append(myTablero[fila][columna]?"o":"x", p);
     }
 }
 
+/* Pinto Celdas de tableroRival */
+for(let fila=0; fila<5; fila++) {
+    for(let columna=0; columna<5; columna++) {
 
-/* Una vez creamos todo el codigo hacemos esto */
-function Cargado(){
+    	var Celda = document.createElement('div');
+		Celda.className = 'Celda';
+		Celda.setAttribute("data-ship-id", myTablero[fila][columna] )
+		ContenedorTableroRival.append(Celda);
 
-	var i=0;
-	var TopeCeldas = document.getElementsByClassName('Celda');
-
-	/* Selecciono todas la celdas */
-	while(i < TopeCeldas.length ){
-
-		/* Aqui DEBERIA añadir la variable i como clase, o como name */
-		console.log(TopeCeldas[i]);
-
-		i++;
-	}
+    	let p = document.createElement('p')
+		Celda.append(tableroRival[fila][columna]?"o":"x", p);
+    }
 }
+
+	let i=0;
+	let TopeCeldas = document.getElementsByClassName('Celda');
+
+}
+
+window.addEventListener("load", Cargado)
