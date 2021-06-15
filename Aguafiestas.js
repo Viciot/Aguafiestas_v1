a09bcea24af545da841989e13866c702e5d55b56
Viciot <vfuentesnoguera@gmail.com>
2021-06-15 19:27:09 +0200

    Changes with Marco

--- Aguafiestas.js
+++ Aguafiestas.js
@@ -6,7 +6,7 @@ const myTablero = [
 	[0, 0, 0, 3, 3],
 	[0, 0, 0, 0, 0]
 ]
-const rivalHits = [] 
+const rivalHits = []
 
 const tableroRival = [
 	[1, 1, 1, 0, 0],
@@ -15,63 +15,71 @@ const tableroRival = [
 	[0, 0, 0, 0, 1],
 	[0, 0, 0, 0, 1]
 ]
-const myHits = [] 
+const myHits = []
 
 
-function iAmHit(event){
-	console.log("You clicked  " + event.target.getAttribute("data-ship-id"))
+function iAmHit(event) {
+	const shipId = event.target.getAttribute("data-ship-id")
+	console.log("You clicked  " + shipId)
+	if (shipId > 0) {
+		rivalHits.push(shipId)
+	}
+	console.log("rivalHits: ", rivalHits)
 }
 
-function rivalIsHit(event){
-	console.log("You clicked  " + event.target.getAttribute("data-ship-id"))
+function rivalIsHit(event) {
+	const shipId = event.target.getAttribute("data-ship-id")
+	console.log("You clicked  " + shipId)
+	if (shipId > 0) {
+		myHits.push(shipId)
+	}
+	event.target.classList.add("hit")
+	console.log("myHits: ", myHits)
 }
 
 /* Una vez creamos todo el codigo hacemos esto */
-function Cargado(){
+function Cargado() {
 
-/* Creo tablero de la izquierda */
-var ContenedorMiTablero = document.createElement('div');
-ContenedorMiTablero.id = 'MiTablero';
-ContenedorMiTablero.addEventListener("click", iAmHit )
-document.body.appendChild(ContenedorMiTablero); 
+	/* Creo tablero de la izquierda */
+	let ContenedorMiTablero = document.createElement('div');
+	ContenedorMiTablero.id = 'MiTablero';
+	ContenedorMiTablero.addEventListener("click", iAmHit)
+	document.body.appendChild(ContenedorMiTablero);
 
-/* Creo tablero de la derecha */
-var ContenedorTableroRival = document.createElement('div');
-ContenedorTableroRival.id = 'TableroRival';
-ContenedorTableroRival.addEventListener("click", rivalIsHit )
-document.body.appendChild(ContenedorTableroRival); 
+	/* Creo tablero de la derecha */
+	let ContenedorTableroRival = document.createElement('div');
+	ContenedorTableroRival.id = 'TableroRival';
+	ContenedorTableroRival.addEventListener("click", rivalIsHit)
+	document.body.appendChild(ContenedorTableroRival);
 
 
-/* Pinto Celdas de MiTablero */
-for(let fila=0; fila<5; fila++) {
-    for(let columna=0; columna<5; columna++) {
+	/* Pinto Celdas de MiTablero */
+	for (let fila = 0; fila < 5; fila++) {
+		for (let columna = 0; columna < 5; columna++) {
 
-    	var Celda = document.createElement('div');
-		Celda.className = 'Celda';
-		Celda.setAttribute("data-ship-id", myTablero[fila][columna] )
-    	ContenedorMiTablero.append(Celda);
+			let Celda = document.createElement('div');
+			Celda.className = 'Celda';
+			Celda.setAttribute("data-ship-id", myTablero[fila][columna])
 
-    	let p = document.createElement('p')
-		Celda.append(myTablero[fila][columna]?"o":"x", p);
-    }
-}
+			let p = document.createElement('p')
+			Celda.append(myTablero[fila][columna] ? "o" : "x", p);
+			ContenedorMiTablero.append(Celda);
+		}
+	}
 
-/* Pinto Celdas de tableroRival */
-for(let fila=0; fila<5; fila++) {
-    for(let columna=0; columna<5; columna++) {
+	/* Pinto Celdas de tableroRival */
+	for (let fila = 0; fila < 5; fila++) {
+		for (let columna = 0; columna < 5; columna++) {
 
-    	var Celda = document.createElement('div');
-		Celda.className = 'Celda';
-		Celda.setAttribute("data-ship-id", myTablero[fila][columna] )
-		ContenedorTableroRival.append(Celda);
+			let Celda = document.createElement('div');
+			Celda.className = 'Celda';
+			Celda.setAttribute("data-ship-id", myTablero[fila][columna])
+			ContenedorTableroRival.append(Celda);
 
-    	let p = document.createElement('p')
-		Celda.append(tableroRival[fila][columna]?"o":"x", p);
-    }
-}
-
-	let i=0;
-	let TopeCeldas = document.getElementsByClassName('Celda');
+			let p = document.createElement('p')
+			Celda.append(tableroRival[fila][columna] ? "o" : "x", p);
+		}
+	}
 
 }
 
